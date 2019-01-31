2767fb24f863af864964da42e5d5735f26a68140
Vika Tarasevich <tarasevich123@yandex.ru>
2019-01-31 23:45:57 +0300

    Updated scrapper

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const puppeteer = require('puppeteer')
 
 const scrapper = require('./scrapper')
 
@@ -7,22 +6,10 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 
-async function getPic() {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-   
-
-    await scrapper.login(page);
-    await scrapper.choosePrimaryProfile(page);
-  
-    await browser.close();
-  }
-
-
 const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
    
-  getPic();
+  await scrapper.runScrapper();
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 
--- scrapper/index.js
+++ scrapper/index.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer')
 
 const { 
   LOGIN_SELECTOR, 
@@ -6,8 +7,23 @@ const {
   PRIMARY_PROFILE_LINK_SELECTOR
 } = require('./selectors.json');
 
+const {
+  LOGIN_LINK,
+  AUDIO_LINK
+} = require('./constants.json')
+
+const runScrapper = async() => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1280, height: 926 });
+
+  await login(page);
+  await choosePrimaryProfile(page);
+  await browser.close();
+}
+
 const login = async (page) => {
-  await page.goto('https://www.netflix.com/by/login');
+  await page.goto(LOGIN_LINK);
 
   await page.click(LOGIN_SELECTOR);
   await page.keyboard.type(process.env.LOGIN);
@@ -23,16 +39,13 @@ const choosePrimaryProfile = async (page) => {
   await page.click(PRIMARY_PROFILE_LINK_SELECTOR);
   await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
 
-  await page.goto('https://www.netflix.com/browse/audio');
+  await page.goto(AUDIO_LINK);
 
   await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 }
 
-
-
 module.exports = {
-  login,
-  choosePrimaryProfile
+  runScrapper
 }
 
 
