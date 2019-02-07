20b5704cf1aefb237d3b2c7ffbfe9e863f9aae15
Vika Tarasevich <tarasevich123@yandex.ru>
2019-02-07 22:45:57 +0300

    Updated selectors

--- scrapper/index.js
+++ scrapper/index.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs');
 
 const { 
   LOGIN_SELECTOR, 
   PASSWORD_SELECTOR, 
   LOGIN_BUTTON_SELECTOR,
-  PRIMARY_PROFILE_LINK_SELECTOR
+  PRIMARY_PROFILE_LINK_SELECTOR,
+  PROFILES_GATE_CONTAINER_SELECTOR
 } = require('./selectors.json');
 
 const {
@@ -13,13 +15,61 @@ const {
 } = require('./constants.json')
 
 const runScrapper = async() => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({headless: false,
+    ignoreHTTPSErrors: true,
+    args:[
+        '--enable-features=NetworkService',
+        '--no-sandbox',
+    ] });
   const page = await browser.newPage();
   page.setViewport({ width: 1280, height: 926 });
 
   await login(page);
   await choosePrimaryProfile(page);
+  await goToAudioPage(page);
+  await changeLanguage(page);
+
+  await autoScroll(page);
+  const items = await page.evaluate(extractItems)
+
+  fs.writeFileSync('items.txt', items.join('\n') + '\n');
+  console.log('done');
   await browser.close();
+}
+
+
+const changeLanguage = async (page) => {
+  await page.click('.languageDropDown');
+  await page.click('.languageDropDown > div > div.sub-menu.theme-lakira > ul > li:nth-child(2) > a');
+  await page.waitFor(5000);
+}
+
+function extractItems() {
+  const extractedElements = document.querySelectorAll('.rowContainer_title_card');
+  const items = [];
+  for (let element of extractedElements) {
+    items.push(element.innerText);
+  }
+  return items;
+}
+
+const autoScroll = async (page) =>{
+  await page.evaluate(async () => {
+      await new Promise((resolve, reject) => {
+          let totalHeight = 0;
+          const distance = 100;
+          const timer = setInterval(() => {
+              var scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+
+              if(totalHeight >= scrollHeight){
+                  clearInterval(timer);
+                  resolve();
+              }
+          }, 100);
+      });
+  });
 }
 
 const login = async (page) => {
@@ -36,12 +86,19 @@ const login = async (page) => {
 }
 
 const choosePrimaryProfile = async (page) => {
-  await page.click(PRIMARY_PROFILE_LINK_SELECTOR);
-  await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
 
-  await page.goto(AUDIO_LINK);
+  const isProfilesGate = await page.evaluate((selector) => { 
+    const elements = document.getElementsByClassName(selector);
+    return !!elements.length;
+  },PROFILES_GATE_CONTAINER_SELECTOR);
 
-  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+  if(isProfilesGate){
+    await page.click(PRIMARY_PROFILE_LINK_SELECTOR);
+  }
+}
+
+const goToAudioPage = async (page) => {
+  await page.goto(AUDIO_LINK, { waitUntil: 'networkidle0' });
 }
 
 module.exports = {
